ff324e0659b6a24696f1737337ae2c77cae4acfb
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2018-11-01 14:56:52 -0200

    pagina agradecimento

--- trabalhoWeb/file.php
+++ trabalhoWeb/file.php
@@ -1,30 +1,15 @@
 <?php
 
-$destinatario = "theylor.marmitt@unoesc.edu.br";
-
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$telefone = $_POST['telefone'];
 $assunto = $_POST['assunto'];
-$obs = $_POST['obs'];
 
+$corpo = "
+	Assunto: "  	.$_POST['assunto']. "
+	Nome: "        .$_POST['nome']. "
+	Email: "       .$_POST['email']. "
+	Telefone: "    .$_POST['telefone']. "
+	Informação: "  .$_POST['obs']. "
+";
 
-$body = "===================================" . "\n";
-$body = $body . "Contato - BestCarAlugueis" . "\n";
-$body = $body . "===================================" . "\n\n";
-$body = $body . "Assunto: " . $assunto . "\n\n";
-$body = $body . "Nome: " . $nome . "\n";
-$body = $body . "Email: " . $email . "\n";
-$body = $body . "Telefone: " . $telefone . "\n\n";
-
-$body = $body . "obs: " . $obs . "\n\n";
-
-$body = $body . "===================================" . "\n";
-
-// envia o email
-mail($destinatario, $assunto , $body, "From: $email\r\n");
-
-header("location:obrigado.htm");
-
+mail('theylor.marmitt@unoesc.edu.br', $assunto , $corpo, 'From: $theylor.marmitt@unoesc.edu.br');
 
 ?>
